162373b2ae557014726cef789f58fdbb78126685
wellmota <impeto.wellington@gmail.com>
2024-03-06 16:53:40 -0300

    creating blog posts using fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postResponses = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photoResponses = fetch("https://jsonplaceholder.typicode.com/photos");
+    const [posts, photos] = await Promise.all([postResponses, photoResponses]);
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return {
+        ...post,
+        cover: photosJson[index].url,
+      };
+    });
+    console.log(postsAndPhotos);
+    this.setState({ posts: postsAndPhotos });
+  };
+
+  componentDidUpdate() {}
+  componentWillUnmount() {}
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1 className="truncate-one-line">{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
